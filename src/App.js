646b66b873cf91701557922d38503dcc6fd334cf
Asus <aldinae@bk.ru>
2022-11-18 17:49:07 +0600

    hello friend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
 
 function App() {
+  const[leftDiceNumber,setLeftDiceNumber]=useState(2);
+    const [rightDiceNumber, setRightDiceNumber] = useState(3);
+
+  const onRightBallClicked = () =>{
+    setRightDiceNumber(Math.floor(Math.random()*5)+1);
+  }
+  const onLeftBallClicked = () =>{
+    setLeftDiceNumber(Math.floor(Math.random()*5)+1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Ball</header>
+      <main>
+        <button onClick={onLeftBallClicked}><img src={require(`./assets/ball${leftDiceNumber}.png`)}/></button>
+        <button onClick={onRightBallClicked}><img src={require(`./assets/ball${rightDiceNumber}.png`)}/></button>
+      </main>
     </div>
   );
 }
+
+
 
 export default App;
